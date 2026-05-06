e5aad2f7a3611435c9537b35b4cb67f4bb09d2dc
Sebastian Iturralde <itusebastian@gmail.com>
2026-05-06 12:45:10 -0500

    Add social media icons and footer; implement current year and last modified date scripts

---
+++ scripts/date.js
@@ -0,0 +1,6 @@
+// Set current year in footer
+const currentYear = new Date().getFullYear();
+document.getElementById('current-year').textContent = currentYear;
+
+// Set last modified date
+document.getElementById('last-modified').textContent = document.lastModified
