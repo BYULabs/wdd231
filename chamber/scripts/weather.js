dc1c7542bcc7f352335c193d6d3966448d97856f
Sebastian Iturralde <itusebastian@gmail.com>
2026-05-21 11:08:10 -0500

    Add weather.js script for current weather data display and error handling

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -16,20 +16,35 @@ const myLon = "-78.4355379";
 
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLon}&appid=${myKey}&units=imperial`;
 
+function displayResults(data) {
+    // Current weather
+    currentTemp.innerHTML = `${Math.round(data.main.temp)}`;
+    currentCondition.textContent = data.weather[0].main;
+    tempHigh.innerHTML = `${Math.round(data.main.temp_max)}`;
+    tempLow.innerHTML = `${Math.round(data.main.temp_min)}`;
+    humidity.innerHTML = `${data.main.humidity}`;
+    sunrise.textContent = `${data.sys.sunrise}`;
+    sunset.textContent = `${data.sys.sunset}`;
+    
+    // Weather emoji
+    const weatherEmoji = data.weather[0].icon;
+    weatherIcon.textContent = weatherEmoji;
+}
+
 async function apiFetch() {
-  try {
-    // Fetch current weather
-    const response = await fetch(url);
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-      displayResults(data);
-    } else {
-      throw Error(await response.text());
+    try {
+        // Fetch current weather
+        const response = await fetch(url);
+        if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        displayResults(data);
+        } else {
+        throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log('Weather API Error:', error);
     }
-  } catch (error) {
-    console.log('Weather API Error:', error);
-  }
 }
 
 // Fetch weather when page loads
